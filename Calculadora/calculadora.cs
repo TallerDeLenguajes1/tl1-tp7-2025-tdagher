a757e2213485e3cb3f4d17d5b963f1cf87e1ac79
tdagher <tomasdaghero99@gmail.com>
2025-05-27 12:58:05 -0300

    agregada funciones de calculo

--- Calculadora/calculadora.cs
+++ Calculadora/calculadora.cs
@@ -1,1 +1,38 @@
 namespace EspacioCalculadora;
+
+public class Calculadora
+{
+    double termino1, termino2, resultado;
+    //METODOS
+    void Sumar(double termino1, double termino2)
+    {
+        resultado = termino1 + termino2;
+    }
+    void Restar(double termino)
+    {
+        resultado = termino1 - termino2;
+    }
+    void Multiplicar(double termino)
+    {
+        resultado = termino1 * termino2;
+    }
+    void Dividir(double termino)
+    {
+        resultado = termino1 / termino2;
+    }
+    void Limpiar()
+    {
+
+    }
+}
+
+
+
+
+
+
+// 
+// ● void Restar(double termino)
+// ● 
+// ● 
+// ● 
